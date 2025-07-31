447c6f74c02ffda38a1a8188acf8a9147bf902fb
espinozaje <jeisson12aaron@gmail.com>
2025-07-31 11:44:42 -0500

    first commit

---
+++ src/main/java/com/example/Qore/QoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.Qore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QoreApplication.class, args);
+	}
+
+}
