d1b39045713e48c761e4c06f9e8b9c2c4bb1ba2c
espinozaje <jeisson12aaron@gmail.com>
2025-10-23 12:32:05 -0500

    deploy

--- src/main/java/com/example/Qore/DataInitializer.java
+++ src/main/java/com/example/Qore/DataInitializer.java
@@ -1,8 +1,10 @@
 package com.example.Qore;
 
+import com.example.Qore.model.Discipline;
 import com.example.Qore.model.Permission;
 import com.example.Qore.model.Plan;
 import com.example.Qore.model.RoleE;
+import com.example.Qore.repository.DisciplineRepository;
 import com.example.Qore.repository.PermissionRepository;
 import com.example.Qore.repository.PlanRepository;
 import com.example.Qore.repository.RoleRepository;
@@ -19,9 +21,26 @@ public class DataInitializer implements CommandLineRunner {
     private final PlanRepository planRepository;
     private final RoleRepository roleRepository;
     private final PermissionRepository permissionRepository;
+    private final DisciplineRepository disciplineRepository;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
+        System.out.println("Iniciando carga de data inicial...");
+
+        try {
+            createPermissionsAndRoles();
+            createPlans();
+            createDisciplines();
+            System.out.println("Data inicial cargada correctamente.");
+        } catch (Exception e) {
+            System.err.println("Error al cargar data inicial: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+
+
+    private void createPermissionsAndRoles() {
         // PERMISOOOOOOOOSSS
         Permission adminPermission = createPermissionIfNotExists("ADMIN_ACCESS");
         Permission instructorPermission = createPermissionIfNotExists("INSTRUCTOR_ACCESS");
@@ -35,13 +54,20 @@ public class DataInitializer implements CommandLineRunner {
         Permission excelPermission = createPermissionIfNotExists("EXCEL_ACCESS");
         Permission paymentPermission = createPermissionIfNotExists("PAYMENT_ACCESS");
         // Roles con sus permisos
-        createRoleIfNotExists("ADMIN","ROL ADMIN" ,Set.of(adminPermission));
-        createRoleIfNotExists("CLIENT","ROL CLIENT" ,Set.of(clientPermission));
-        createRoleIfNotExists("INSTRUCTOR","ROL INSTRUCTOR" ,Set.of(instructorPermission));
-        createRoleIfNotExists("STAFF","ROL STAFF" ,Set.of(staffPermission));
-        createRoleIfNotExists("MANAGER","ROL MANAGER" ,Set.of(managerPermission));
+        createRoleIfNotExists("ADMIN", "ROL ADMIN", Set.of(adminPermission));
+        createRoleIfNotExists("CLIENT", "ROL CLIENT", Set.of(clientPermission));
+        createRoleIfNotExists("INSTRUCTOR", "ROL INSTRUCTOR", Set.of(instructorPermission));
+        createRoleIfNotExists("STAFF", "ROL STAFF", Set.of(staffPermission));
+        createRoleIfNotExists("MANAGER", "ROL MANAGER", Set.of(managerPermission));
+
+    }
 
 
+    private void createPlans() {
+        if (planRepository.count() > 0) {
+            System.out.println("📦 Planes ya existentes. No se insertan duplicados.");
+            return;
+        }
         //CREACION DE PLANES
         if (planRepository.count() == 0) {
             Plan plan1 = Plan.builder()
@@ -130,6 +156,48 @@ public class DataInitializer implements CommandLineRunner {
 
             planRepository.saveAll(List.of(plan1, plan2, plan3, plan4, plan5, plan6, plan7));
         }
+    }
+
+    private void createDisciplines() {
+        if (disciplineRepository.count() > 0) {
+            System.out.println("🧘‍♀️ Disciplinas ya existentes. No se insertan duplicados.");
+            return;
+        }
+
+        System.out.println("🧘‍♀️ Insertando disciplinas iniciales...");
+
+        List<Discipline> disciplines = List.of(
+                Discipline.builder()
+                        .name("Pre Pilates")
+                        .description("Para alumnas principiantes que recién están teniendo un acercamiento con la disciplina.")
+                        .build(),
+                Discipline.builder()
+                        .name("Pilates Principiante")
+                        .description("Para alumnas que ya tienen claros los principios básicos y están reconociendo su cuerpo.")
+                        .build(),
+                Discipline.builder()
+                        .name("Pilates Intermedio")
+                        .description("Para alumnas que ya conocen y dominan los principios básicos del Pilates.")
+                        .build(),
+                Discipline.builder()
+                        .name("Pilates Avanzado")
+                        .description("Para alumnas que dominan los principios del Pilates y dominan también su cuerpo para poder sostenerse.")
+                        .build(),
+                Discipline.builder()
+                        .name("Pilates Rehabilitación")
+                        .description("Para alumnas que por distintas condiciones o patologías necesitan de especial atención y cuidado.")
+                        .build(),
+                Discipline.builder()
+                        .name("Pilates Pre y Post Natal")
+                        .description("Para alumnas que están embarazadas o recién han dado a luz (4 meses).")
+                        .build(),
+                Discipline.builder()
+                        .name("Pilates Senior")
+                        .description("Para mayores de 70 años.")
+                        .build()
+        );
+
+        disciplineRepository.saveAll(disciplines);
     }
 
     private Permission createPermissionIfNotExists(String name) {
